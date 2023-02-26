d75e4c678bfbc7b553779c715a92bb2cfff37a42
Hema Vasupalli <94504465+hemavasupalli@users.noreply.github.com>
2023-02-26 01:18:13 -0500

    Add files via upload

---
+++ src/components/WatchPage.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import {  useSearchParams} from 'react-router-dom';
+import { closeMenu } from '../utils/appSlice';
+import Comments from './Comments';
+
+const WatchPage = () => {
+  const [searchParams]= useSearchParams();
+  const dispatch =useDispatch();
+  useEffect(()=>{
+    dispatch(closeMenu());
+  })
+  return (
+    <>
+    <div className='flex flex-col px-7'>
+        <iframe width="1200" height="600" 
+        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>
+
+        </iframe>
+    
+    <Comments />
+    </div>
+    </>
+  )
+}
+
+export default WatchPage
